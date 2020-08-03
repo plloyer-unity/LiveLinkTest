dea08600e5c33269a6bbdeeea57727e5812fc284
Pierre-Luc Loyer <pierreluc.loyer@unity3d.com>
2020-08-03 11:45:33 -0400

    Creation of entity is now properly linked with game object

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -18,22 +18,26 @@ public class TestScript : MonoBehaviour
 
         // Create matching Entity
         var world = World.DefaultGameObjectInjectionWorld;
-        CreateEntity(obj, world);
+        var sceneGuid = new GUID(AssetDatabase.AssetPathToGUID(gameObject.scene.path));
+        var sceneSystem = world.GetExistingSystem<SceneSystem>();
+        var sceneEntity = sceneSystem.GetSceneEntity(sceneGuid);
+        var sectionEntity = world.EntityManager.GetBuffer<ResolvedSectionEntity>(sceneEntity)[0].SectionEntity;
+
+        CreateEntity(obj, world, sectionEntity, sceneGuid);
         foreach (var w in World.All)
         {
             if (w.Name.StartsWith("Converted"))
-                CreateEntity(obj, w);
+                CreateEntity(obj, w, sectionEntity, sceneGuid);
         }
-        
 
         // Register object for conversion (triggers a live-link update)
         Undo.RegisterCreatedObjectUndo(obj, "Create Object");
     }
 
-    void CreateEntity(GameObject obj, World world)
+    void CreateEntity(GameObject obj, World world, Entity sectionEntity, GUID sceneGuid)
     {
         Debug.Log("Added entity to world: " + world.Name);
-        
+
         var manager = world.EntityManager;
         var entity = manager.CreateEntity();
         manager.SetName(entity, "MyEntity");
@@ -45,10 +49,6 @@ public class TestScript : MonoBehaviour
         manager.AddComponentData(entity, new Rotation{ Value = obj.transform.localRotation });
 
         // Set the SceneTag and SceneSection on the entity so it's in the sub-scene
-        var sceneGuid = new GUID(AssetDatabase.AssetPathToGUID(gameObject.scene.path));
-        var sceneSystem = world.GetExistingSystem<SceneSystem>();
-        var sceneEntity = sceneSystem.GetSceneEntity(sceneGuid);
-        var sectionEntity = manager.GetBuffer<ResolvedSectionEntity>(sceneEntity)[0].SectionEntity;
         manager.AddSharedComponentData(entity, new SceneTag{ SceneEntity = sectionEntity });
         manager.AddSharedComponentData(entity, new SceneSection { SceneGUID = sceneGuid });
     }
